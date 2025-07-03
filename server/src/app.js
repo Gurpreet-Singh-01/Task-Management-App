9c6f9f7b9918afe33c2d5b912b97e60a2fa68374
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-07-03 18:27:31 +0530

    Created User Model

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middlewares/errorMiddleware");
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
@@ -11,4 +12,6 @@ app.use(express.json({limit:'16kb'}));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cookieParser());
 
+
+app.use(errorMiddleware)
 module.exports = app;
---
+++ server/src/models/User.model.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+      maxlength: 100,
+    },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hashing Password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+// Matching Password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// JWT TOKENS
+userSchema.methods.accessToken = function () {
+  return JWT.sign(
+    { id: this._id, username: this.username },
+    process.env.ACCESS_TOKEN_JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+};
+
+userSchema.methods.refreshToken = function () {
+  return JWT.sign({ id: this._id }, process.env.REFRESH_TOKEN_JWT_SECRET, {
+    expiresIn: "3d",
+  });
+};
+
+const User = model("User", userSchema);
+module.exports = User;
