6d69799f193e4277cd3d3b006e37d450fa7d1c12
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-07-09 09:18:55 +0530

    Task Model

---
+++ server/src/models/Task.model.js
@@ -0,0 +1,32 @@
+const { Schmea, model, Schema } = require("mongoose");
+
+const TaskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["todo", "in-progress", "done"],
+      default: "todo",
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    attachment: {
+      data: { type: Buffer },
+      contentType: { type: String },
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = model("Task", TaskSchema);
+module.exports = Task;
